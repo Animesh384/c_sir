146aa541b08f74e2aae9c10dc034afed47088c45
akanoob <animeshk384@gmail.com>
2022-02-21 13:24:44 +0530

    wow

---
+++ 21_2_22.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+
+int main(){
+	int a,b;
+	a=10;
+	b=20;
+	printf("sum %d",(a+b));
+	printf("product %d",(a*b));
+	printf("modules %d",(a%b));
+	printf("division %d",(a/b));
+}
+
+
