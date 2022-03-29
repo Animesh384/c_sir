f9e95bb94b57c20cb3f9cec52caff39b454fb033
akanoob <animeshk384@gmail.com>
2022-03-29 10:47:06 +0530

    update

--- lab.10/q4.c
+++ lab.10/q4.c
@@ -1,5 +1,47 @@
+/*
+     1
+    222
+   33333
+  4444444
+
+
+
+
+
+*/
+
 #include<stdio.h>
 int main(){
-    int k=10;
-    float g=2;
-}
+     int m;
+    printf("Enter a Number :");
+    scanf("%d",&m);
+     for(int i=0;i<m;i++){
+        for(int j=0;j<(m-1-i);j++)
+            printf(" ");
+        for(int j=0;j<(i*2+1);j++)
+        printf("%d",i+1);
+        for(int j=0;j<(m-1-i);j++)
+            printf(" ");
+        for(int j=0;j<(m-1-i);j++)
+            printf(" ");
+        for(int j=0;j<(i*2+1);j++)
+        printf("%d",i+1);
+
+        printf("\n");
+        
+            }
+     
+    for(int j=m-1;j>=1;j--){
+        for(int i=(m-j);i>=1;i--)
+        printf(" ");
+        for(int i=(j*2-1);i>=1;i--)
+        printf("%d",j);
+        for(int i=(m-j);i>=1;i--)
+        printf(" ");
+        for(int i=(m-j);i>=1;i--)
+        printf(" ");
+        for(int i=(j*2-1);i>=1;i--)
+        printf("%d",j);  
+        printf("\n");      
+    }
+}        
---
+++ lab.10/tempCodeRunnerFile.c
@@ -0,0 +1,8 @@
+ }
+    for(int j=m-1;j>=1;j--){
+        for(int i=j;i>=1;i--)
+        printf("$");
+        for(int i=(m-j);i>=1;i--)
+        printf(" ");
+        for(int i=j;i>=1;i--)
+        printf("$");
