777204d02e2c2ecc006f6e4f1353d6b43c98837b
akanoob <animeshk384@gmail.com>
2022-04-13 13:01:35 +0530

    lab 16

--- lab.16/q1.c
+++ lab.16/q1.c
@@ -7,9 +7,10 @@ int main(){
     int a2[3][3];
 
 
-    int sum=0;
+    int sum;
     for(int i=0;i<3;i++){
         for(int k=0;k<3;k++){
+            sum=0;
         for(int j=0;j<3;j++){
             sum=sum+a[i][j]*a1[j][i];
         }
