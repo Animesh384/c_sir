2acdd31828e27c06ed3a27dfd523e0e02016c336
akanoob <animeshk384@gmail.com>
2022-04-13 23:11:20 +0530

    lab 16

--- lab.16/q2.c
+++ lab.16/q2.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
+#include<limits.h>
 
 int main(){
- int a[3][3]={{1,2,3},{4,10,6},{7,8,9}};
- int max=0;
+ int a[3][3]={{-1111,2,3},{4,-10,6},{7,-8,-9}};
+ int max=INT_MIN;
 
  for(int i=0;i<3;i++){
   if(max<a[i][i]) max=a[i][i];
