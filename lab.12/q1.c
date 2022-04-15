4510a4618f9cc76d8d460a17850380f898b9e081
akanoob <animeshk384@gmail.com>
2022-04-16 03:31:17 +0530

    lab 12

---
+++ lab.12/q1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void uchar(char ch){
+    if((int)ch>=97&&(int)ch<=122){
+        ch=ch-32;
+        printf("char=%c",ch);
+    }
+
+}
+
+int main(){
+    char ch;
+    printf("enter character :");
+    scanf("%c",&ch);
+    uchar(ch);
+
+
+}
